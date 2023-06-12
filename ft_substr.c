61603dcfc820a8d3bee49feca3cf941ea5cbae26
ymwsimon <ymwsimon@gmail.com>
2023-06-12 19:43:50 +0100

    correct substr

--- ft_substr.c
+++ ft_substr.c
@@ -16,16 +16,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (0);
 	slen = ft_strlen(s);
-	/*if (len && slen && start < slen)
-		res = malloc(ft_min(len, slen - start) + 1);
-	else
-		res = malloc(1);*/
-	slen = ft_min(slen, slen - start) + 1;
-	res = malloc(ft_min(len, slen) + 1);
+	if (start >= slen)
+		return (ft_strdup(""));
+	slen = ft_min(slen - start, len);
+	res = malloc(slen + 1);
 	if (res)
 	{
 		i = 0;
-		while (i < len && (start + i) < slen)
+		while (i < slen)
 		{
 			res[i] = s[start + i];
 			i++;

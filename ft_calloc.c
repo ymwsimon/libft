b5a17a6325653b395915f788f04de4ff0016149e
ymwsimon <ymwsimon@gmail.com>
2023-07-28 16:16:33 +0100

    new calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -4,9 +4,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*t;
 	size_t	i;
+	const uint maxsize = -1;
 
 	t = 0;
-	if ((nmemb <= 4294967295 || size <= 4294967295) && nmemb * size <= 4294967295)
+	if ((nmemb <= maxsize || size <= maxsize) && nmemb * size <= maxsize)
 		t = malloc(nmemb * size);
 	i = 0;
 	if (t)
